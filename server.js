a07cbb936c725668d29d4cb39b59f6eb7478efd6
John-Kassah <johnkassahwebdev@gmail.com>
2025-07-30 11:46:21 +0000

    Added a domain/source to the cors policy

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(express.json());
 // and allow your app to send requests to other domains and ports. This is important for security reasons.
 app.use(cors({
 
-    origin: ['https://rouje-naturel.vercel.app', 'http://localhost:5173'], // Your frontend's URL
-
+    origin: ['https://rouje-naturel.vercel.app', 'http://localhost:5173', 'https://www.roujenaturel.com/'], // Your frontend's URL
     credentials: true
+
 }));
 
 // Defininf a port number
